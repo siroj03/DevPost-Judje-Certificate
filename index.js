4db75478a29be24bcbd336b75e1acee81c2a0190
Siroj <kobilov02.02@mail.ru>
2024-11-02 16:51:42 +0300

    now able to edit ceritficate in real time

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const userName = document.getElementById("user_name");
+const hackathon = document.getElementById("hackathon");
+console.log(hackathon.innerText);
+const date = document.getElementById("date");
+const date_issue = document.getElementById("date_issue");
 const submitBtn = document.getElementById("submitBtn");
-console.log(userName.value);
 const { PDFDocument, rgb, degrees } = PDFLib;
 
 
@@ -11,18 +14,21 @@ const capitalize = (str, lower = false) =>
 
 submitBtn.addEventListener("click", () => {
   const val = capitalize(userName.value);
+  const hackathon_name = hackathon.innerText;
+  const date_value = date.value;
+  const date_issue_value = date_issue.value;
 
   //check if the text is empty or not
   if (val.trim() !== "" && userName.checkValidity()) {
     // console.log(val);
-    generatePDF(val);
+    generatePDF(val, hackathon_name, date_value, date_issue_value);
   } else {
     userName.reportValidity();
   }
 });
 
-const generatePDF = async (name) => {
-  const existingPdfBytes = await fetch("./cert_template.pdf").then((res) =>
+const generatePDF = async (name, hackathon_name, date_value, date_issue_value) => {
+  const existingPdfBytes = await fetch("./new_temp.pdf").then((res) =>
     res.arrayBuffer()
   );
 
@@ -31,7 +37,7 @@ const generatePDF = async (name) => {
   pdfDoc.registerFontkit(fontkit);
 
   //get font
-  const fontBytes = await fetch("./Sanchez-Regular.ttf").then((res) =>
+  const fontBytes = await fetch("./font/Sanchez-Regular.ttf").then((res) =>
     res.arrayBuffer()
   );
 
@@ -48,9 +54,35 @@ const generatePDF = async (name) => {
 
   // Draw a string of text diagonally across the first page
   firstPage.drawText(name, {
-    x: 225 - (len / 2)*20,
-    y: 200,
+    x: 500 - (len / 2)*20,
+    y: 400,
     size: 58,
+    font: SanChezFont,
+    color: rgb(0.2, 0.84, 0.67),
+  });
+
+
+  firstPage.drawText(hackathon_name, {
+    x: 600 - (len / 2)*20,
+    y: 270,
+    size: 30,
+    font: SanChezFont,
+    color: rgb(0.2, 0.84, 0.67),
+  });
+
+
+  firstPage.drawText(date_value, {
+    x: 693 - (len / 2)*20,
+    y: 226,
+    size: 20,
+    font: SanChezFont,
+    color: rgb(0.2, 0.84, 0.67),
+  });
+
+  firstPage.drawText(date_issue_value, {
+    x: 1040 - (len / 2)*20,
+    y: 105,
+    size: 20,
     font: SanChezFont,
     color: rgb(0.2, 0.84, 0.67),
   });
@@ -66,7 +98,7 @@ const generatePDF = async (name) => {
 
   var file = new File(
     [pdfBytes],
-    "Padhega India Subscription Certificate.pdf",
+    "DevPost Judge Certificate.pdf",
     {
       type: "application/pdf;charset=utf-8",
     }
@@ -75,3 +107,17 @@ const generatePDF = async (name) => {
 };
 
 // init();
+
+// Add or Remove Winners
+const addWinnersBtn = document.getElementById("add-winners-btn");
+const winnersContainer = document.querySelector(".winners-container");
+
+addWinnersBtn.addEventListener("click", () => {
+  if (addWinnersBtn.innerText === "Add Winners") {
+    winnersContainer.style.display = "block";
+    addWinnersBtn.innerText = "Remove Winners";
+  } else {
+    winnersContainer.style.display = "none";
+    addWinnersBtn.innerText = "Add Winners";
+  }
+});
